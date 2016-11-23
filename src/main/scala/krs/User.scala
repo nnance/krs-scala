2a9fdcd6238f929b7209276d5e9f060a99e76463
Craig Giles <craig.giles@creditkarma.com>
2016-11-22 16:31:20 -0800

    Comment

--- src/main/scala/krs/User.scala
+++ src/main/scala/krs/User.scala
@@ -1,3 +1,5 @@
 package krs
 
+// Would suggest that user becomes a case class instead of 'class' .. its not a
+// major thing at all, but it has added benefits that normal classes do not.
 class User(val name: String, val creditScore: Short, val outstandingLoanAmount: Double)
