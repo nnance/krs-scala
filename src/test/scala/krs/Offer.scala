9d113dd532fd6f406ea41318a4734cfcb0ca7e6c
Nick Nance <nance.nick@gmail.com>
2016-11-26 19:25:42 -0600

    complete offer and eligibility tests

--- src/test/scala/krs/Offer.scala
+++ src/test/scala/krs/Offer.scala
@@ -43,7 +43,7 @@ class MaxLoanAmountSpec extends FlatSpec with Matchers {
   }
 }
 
-class CreditCardSpec extends FlatSpec with Matchers {
+class CreditCardEligibilitySpec extends FlatSpec with Matchers {
   val ccOffer = CreditCard("Chase", Range(500, 700))
 
   "isEligable" should "be true for user with score within range" in {
@@ -55,7 +55,7 @@ class CreditCardSpec extends FlatSpec with Matchers {
   }
 }
 
-class PersonalLoanSpec extends FlatSpec with Matchers {
+class PersonalLoanEligibilitySpec extends FlatSpec with Matchers {
   val plOffer = PersonalLoan("Chase", Range(500, 700), 400.00, 12)
 
   "isEligable" should "be true for user with score within range" in {
@@ -73,9 +73,13 @@ class PersonalLoanSpec extends FlatSpec with Matchers {
 
 class FilterEligibleRulesSpec extends FlatSpec with Matchers {
   // Offer01 should be the only thing that user01 is eligible for
-  val user01 = new User("TestUser01", 500, 0.0)
-  // Offer06 and Offer07 should be the only thing that user02 is eligible for
-  val user02 = new User("TestUser02", 765, 0.0)
+  val user01 = new User("TestUser01", 500, 100.00)
+  // Offer05 and Offer06 should be the only thing that user02 is eligible for
+  val user02 = new User("TestUser02", 765, 100.00)
+  // Offer01 and Offer09 should be the only thing that user03 is eligible for
+  val user03 = new User("TestUser03", 500, 0.00)
+  // Offer05, Offer06, offer10 should be the only thing that user04 is eligible for
+  val user04 = new User("TestUser04", 765, 0.00)
 
   val offers = Seq(
     CreditCard("Offer01", Range(500, 700)),
@@ -83,7 +87,11 @@ class FilterEligibleRulesSpec extends FlatSpec with Matchers {
     CreditCard("Offer03", Range(600, 700)),
     CreditCard("Offer04", Range(650, 700)),
     CreditCard("Offer05", Range(700, 770)),
-    CreditCard("Offer06", Range(750, 770))
+    CreditCard("Offer06", Range(750, 770)),
+    PersonalLoan("Offer07", Range(500, 700), 0.00, 12),
+    PersonalLoan("Offer08", Range(550, 700), 0.00, 12),
+    PersonalLoan("Offer09", Range(500, 700), 100.00, 12),
+    PersonalLoan("Offer10", Range(750, 770), 100.00, 12)
   )
 
   "Offer eligibilityRules length" should "be 1 for low scoreband user" in {
@@ -95,5 +103,18 @@ class FilterEligibleRulesSpec extends FlatSpec with Matchers {
   "Offer eligibilityRules length" should "be 2 for high scoreband user" in {
     val eligibleOffers = filterEligible(user02, offers)
     eligibleOffers.size should be(2)
+    eligibleOffers.head.provider should be("Offer05")
+  }
+
+  "Offer eligibilityRules length" should "be 2 for no outstanding loans" in {
+    val eligibleOffers = filterEligible(user03, offers)
+    eligibleOffers.size should be(2)
+    eligibleOffers.head.provider should be("Offer01")
+  }
+
+  "Offer eligibilityRules length" should "be 3 for no outstanding loans" in {
+    val eligibleOffers = filterEligible(user04, offers)
+    eligibleOffers.size should be(3)
+    eligibleOffers.head.provider should be("Offer05")
   }
 }
