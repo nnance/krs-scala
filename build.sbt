a546bda75ca7d9e278f92d437760b3a480e3f35d
Nick Nance <nance.nick@gmail.com>
2016-12-08 05:34:25 -0800

    convert to twitter server

--- build.sbt
+++ build.sbt
@@ -8,16 +8,12 @@ scalariformSettings
 
 ensimeIgnoreMissingDirectories := true
 
-val finagleVersion = "6.40.0"
-
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.0",
   "org.scalatest" %% "scalatest" % "3.0.0" % "test",
   "org.json4s" %% "json4s-native" % "3.5.0",
   "org.json4s" %% "json4s-jackson" % "3.5.0",
-  "com.twitter" %% "finagle-http" % finagleVersion,
-  "com.twitter" %% "finagle-thrift" % finagleVersion,
-  "com.twitter" %% "finagle-thriftmux" % finagleVersion,
+  "com.twitter" %% "twitter-server" % "1.25.0",
   "com.twitter" %% "scrooge-core" % "4.12.0",
   "org.apache.thrift" % "libthrift" % "0.9.3"
 )
--- src/main/scala/krs/service/PartnerServer.scala
+++ src/main/scala/krs/service/PartnerServer.scala
@@ -2,11 +2,12 @@ package krs.service
 
 import com.twitter.util.{ Await, Future }
 import com.twitter.finagle.Thrift
+import com.twitter.server.TwitterServer
 
 import krs.thriftscala.{ PartnerService, PartnerOffer, OfferResponse }
 import krs.PartnerSystem._
 
-object PartnerServer extends App {
+object PartnerServer extends TwitterServer {
   def buildServer(): PartnerService[Future] = {
     new PartnerService[Future] {
       val offers = loadOffers(readFile("./fixtures/data.json"))
@@ -22,7 +23,12 @@ object PartnerServer extends App {
     }
   }
 
-  val service = buildServer()
-  val server = Thrift.server.serveIface("localhost:8081", service)
-  Await.ready(server)
+  def main(): Unit = {
+    val service = buildServer()
+    val server = Thrift.server.serveIface("localhost:8081", service)
+    onExit {
+      server.close()
+    }
+    Await.ready(server)
+  }
 }
