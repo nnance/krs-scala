67a504e3e9409048298754ac10353e36f8c3295b
Nick Nance <nick.nance@creditkarma.com>
2016-12-12 14:44:09 -0800

    working finch server

--- src/main/scala/krs/service/PartnerClient.scala
+++ src/main/scala/krs/service/PartnerClient.scala
@@ -1,51 +1,44 @@
 package krs.service
 
 import com.twitter.app.Flag
-import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.{ Http, Service }
+import com.twitter.finagle.http.{ Request, Response }
 import com.twitter.finagle.stats.Counter
 import com.twitter.server.TwitterServer
+
 import io.finch._
+import io.finch.circe._
+import io.circe.generic.auto._
 
 import com.twitter.finagle.Thrift
 
-import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
+// import org.json4s.JsonDSL._
+// import org.json4s.jackson.JsonMethods._
 
 import com.twitter.util.{ Await, Future }
 
-import krs.thriftscala.{ PartnerService, PartnerOffer }
+import krs.thriftscala.{ PartnerService, PartnerOffer, OfferResponse }
 
 object PartnerClient extends TwitterServer {
 
   val port: Flag[Int] = flag("port", 8080, "TCP port for HTTP server")
 
   val todos: Counter = statsReceiver.counter("partnerclient")
+
+  case class Offer(partner: String, minimumScore: Int, maximumScore: Int)
+
   val client: PartnerService.FutureIface =
     Thrift.client.newIface[PartnerService.FutureIface]("localhost:8081", classOf[PartnerService.FutureIface])
-  //
-  // val service = new Service[http.Request, http.Response] {
-  //   def apply(req: http.Request): Future[http.Response] = {
-  //     client.getOffers().map((response) => {
-  //       val json =
-  //         ("offers" ->
-  //           response.offers.map {
-  //             case PartnerOffer(provider, Some(min), Some(max)) =>
-  //               Some((
-  //                 ("provider" -> provider) ~
-  //                 ("minScore" -> min) ~
-  //                 ("maxScore" -> max)
-  //               ))
-  //             case _ => None
-  //           }.flatten)
-  //       val httpResponse = http.Response(req.version, http.Status.Ok)
-  //       httpResponse.setContentString(compact(render(json)))
-  //       httpResponse
-  //     })
-  //   }
-  // }
-  def getOffers: Endpoint[List[PartnerOffer]] = get("offers") {
-    client.getOffers().map(Ok)
+
+  def getOffers: Endpoint[Seq[Offer]] = get("offers") {
+    client.getOffers().map((response) => {
+      val offers = response.offers.map {
+        case PartnerOffer(provider, Some(min), Some(max)) =>
+          Some(Offer(provider, min, max))
+        case _ => None
+      }.flatten
+      Ok(offers)
+    })
   }
 
   val api: Service[Request, Response] = (
@@ -53,14 +46,14 @@ object PartnerClient extends TwitterServer {
   ).toServiceAs[Application.Json]
 
   def main(): Unit = {
-      log.info("Serving the Todo application")
+    log.info("Serving the Client application")
 
-      val server = Http.server
-        .withStatsReceiver(statsReceiver)
-        .serve(s":${port()}", api)
+    val server = Http.server
+      .withStatsReceiver(statsReceiver)
+      .serve(s":${port()}", api)
 
-      onExit { server.close() }
+    onExit { server.close() }
 
-      Await.ready(adminHttpServer)
-    }
+    Await.ready(server)
   }
+}
