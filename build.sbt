80905b0d998efb311c6d3232327e3b4473893cda
Nick Nance <nance.nick@gmail.com>
2016-12-12 07:49:26 -0800

    initial version (wip)

--- build.sbt
+++ build.sbt
@@ -12,7 +12,8 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.0",
   "org.scalatest" %% "scalatest" % "3.0.0" % "test",
   "org.json4s" %% "json4s-native" % "3.5.0",
-  "org.json4s" %% "json4s-jackson" % "3.5.0",
+  "com.github.finagle" %% "finch-core" % "0.11.0-M4",
+  "com.github.finagle" %% "finch-json4s" % "0.11.0-M4",
   "com.twitter" %% "twitter-server" % "1.25.0",
   "com.twitter" %% "scrooge-core" % "4.12.0",
   "org.apache.thrift" % "libthrift" % "0.9.3"
--- src/main/scala/krs/service/PartnerClient.scala
+++ src/main/scala/krs/service/PartnerClient.scala
@@ -1,7 +1,12 @@
 package krs.service
 
-import com.twitter.finagle.{ Http, Service }
-import com.twitter.finagle.http
+import com.twitter.app.Flag
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.stats.Counter
+import com.twitter.server.TwitterServer
+import io.finch._
+
 import com.twitter.finagle.Thrift
 
 import org.json4s.JsonDSL._
@@ -11,30 +16,51 @@ import com.twitter.util.{ Await, Future }
 
 import krs.thriftscala.{ PartnerService, PartnerOffer }
 
-object PartnerClient extends App {
+object PartnerClient extends TwitterServer {
+
+  val port: Flag[Int] = flag("port", 8080, "TCP port for HTTP server")
+
+  val todos: Counter = statsReceiver.counter("partnerclient")
   val client: PartnerService.FutureIface =
     Thrift.client.newIface[PartnerService.FutureIface]("localhost:8081", classOf[PartnerService.FutureIface])
-
-  val service = new Service[http.Request, http.Response] {
-    def apply(req: http.Request): Future[http.Response] = {
-      client.getOffers().map((response) => {
-        val json =
-          ("offers" ->
-            response.offers.map {
-              case PartnerOffer(provider, Some(min), Some(max)) =>
-                Some((
-                  ("provider" -> provider) ~
-                  ("minScore" -> min) ~
-                  ("maxScore" -> max)
-                ))
-              case _ => None
-            }.flatten)
-        val httpResponse = http.Response(req.version, http.Status.Ok)
-        httpResponse.setContentString(compact(render(json)))
-        httpResponse
-      })
-    }
+  //
+  // val service = new Service[http.Request, http.Response] {
+  //   def apply(req: http.Request): Future[http.Response] = {
+  //     client.getOffers().map((response) => {
+  //       val json =
+  //         ("offers" ->
+  //           response.offers.map {
+  //             case PartnerOffer(provider, Some(min), Some(max)) =>
+  //               Some((
+  //                 ("provider" -> provider) ~
+  //                 ("minScore" -> min) ~
+  //                 ("maxScore" -> max)
+  //               ))
+  //             case _ => None
+  //           }.flatten)
+  //       val httpResponse = http.Response(req.version, http.Status.Ok)
+  //       httpResponse.setContentString(compact(render(json)))
+  //       httpResponse
+  //     })
+  //   }
+  // }
+  def getOffers: Endpoint[List[PartnerOffer]] = get("offers") {
+    client.getOffers().map(Ok)
   }
 
-  Await.ready(Http.serve(":8080", service))
-}
+  val api: Service[Request, Response] = (
+    getOffers
+  ).toServiceAs[Application.Json]
+
+  def main(): Unit = {
+      log.info("Serving the Todo application")
+
+      val server = Http.server
+        .withStatsReceiver(statsReceiver)
+        .serve(s":${port()}", api)
+
+      onExit { server.close() }
+
+      Await.ready(adminHttpServer)
+    }
+  }
